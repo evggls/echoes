a196bdd9a1f97daad4698a644c974e3f3f97a9b4
evggls <evgeny.glasu@yandex.ru>
2021-11-20 06:54:14 +0200

    Echoes step 1

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const PORT = 5555;
 const axios = require('axios');
 let items = [];
-
 function startApp(PORT) {
     try {
         app.listen(PORT, () => {
@@ -13,11 +12,7 @@ function startApp(PORT) {
         console.log(err)
     }
 }
-
 app.use('/static', express.static(__dirname + '/static'));
-
-startApp(PORT);
-
 axios.get('https://api.eve-echoes-market.com/market-stats/stats.csv')
     .then(response => {
         let allData = response.data.split('\r\n');
@@ -31,19 +26,82 @@ axios.get('https://api.eve-echoes-market.com/market-stats/stats.csv')
                 buyPrice: item[4],
                 lowestSellPrice: item[4],
                 highestBuyPrice: item[5]
-            })
+            });
         })
-        console.log('Data loaded.')
-
+        console.log('Data loaded.');
+        items.pop()
+        for (let item of items) {
+            if(item.name.toLowerCase().includes('blueprint')) {
+                item.type = 'blueprint';
+            }
+            else if(item.name.toLowerCase().includes('skin')) {
+                item.type = 'skin';
+            }
+            else {
+                item.type = 'item';
+            }
+        }
+        startApp(PORT);
         app.get('/', (req, res) => {
-            console.log(`Request URL: ${req.url}`)
             res.sendFile(__dirname + '/index.html');
+            console.log(`Request URL: "${req.url}", index.html sent`);
         });
-
         app.get('/items/all', (req, res) => {
             console.log(`Request URL: ${req.url}`)
-
             res.send(JSON.stringify(items));
+        });
+        app.get('/items', (req, res) => {
+            console.log(req.url)
+            let itemsByQuery = [];
+            if (req.query.type === 'ships') {
+                if (req.query.race) {
+                    switch (req.query.race) {
+                        case 'Caldari': {
+                            for (let item of items) {
+                                if (item.id[8] === '1' && item.type === 'item') {
+                                    itemsByQuery.push(item);
+                                }
+                            }
+                            break;
+                        }
+                        case 'Minmatar': {
+                            for (let item of items) {
+                                if (item.id[8] === '2' && !item.name.includes('blueprint') && !item.name.includes('skin')) {
+                                    itemsByQuery.push(item);
+                                }
+                            }
+                            break;
+                        }
+                        case 'Amarr': {
+                            for (let item of items) {
+                                if (item.id[8] === '3' && !item.name.includes('skin') && !item.name.includes('blueprint')) {
+                                    itemsByQuery.push(item);
+                                }
+                            }
+                            break;
+                        }
+                        case 'Gallente': {
+                            for (let item of items) {
+                                if (item.id[8] === '4' && !item.name.includes('skin') && !item.name.includes('blueprint')) {
+                                    itemsByQuery.push(item);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (req.query.name) {
+                for (let item of items) {
+                    if (item.name.toLowerCase().includes(req.query.name.toLowerCase())) {
+                        if (req.query.blueprints && item.type === 'blueprint') itemsByQuery.push(item)
+                        if(req.query.skins && item.type === 'skin') itemsByQuery.push(item)
+                        if (item.type === 'item') itemsByQuery.push(item)
+                    }
+                }
+                console.log(itemsByQuery)
+            }
+            res.send(JSON.stringify(itemsByQuery));
         });
 
     })
--- static/js/script.js
+++ static/js/script.js
@@ -2,21 +2,47 @@ let loadBtn = document.querySelector('#loadButton'),
     inputField = document.querySelector('#itemName'),
     allItems = document.querySelector('#allItems'),
     output = document.querySelector('.outputList'),
-    arrowIcon = document.querySelector('.arrowIcon');
+    raceSelector = document.querySelector('#race'),
+    itemType = document.querySelector('#itemType'),
+    bps = document.querySelector('#bp'),
+    skins = document.querySelector('#skins');
 
-arrowIcon.addEventListener('click', () => {
-    arrowIcon.classList.toggle('rotateZ90');
-    document.querySelector('.shipsList').classList.toggle('visuallyHidden')
+raceSelector.addEventListener('change', () => {
+    inputField.value = '';
 })
+
+itemType.addEventListener('change', () => {
+    inputField.value = '';
+})
+
+
 loadBtn.addEventListener('click', () => {
     output.innerHTML = '';
 
-    let reqURL = `http://localhost:5555/items/`;
-    if (allItems.checked) {
-        reqURL += 'all'
-    }
+    let reqURL = `http://localhost:5555/items`;
+
     let req = new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
+
+        if (allItems.checked) {
+            reqURL += '/all'
+        } else if (inputField.value) {
+            reqURL += `?name=${inputField.value}`;
+            if(bps.checked) {
+                reqURL += `&blueprints=true`;
+            }
+            if(skins.checked) {
+                reqURL += `&skins=true`;
+            }
+
+
+        } else {
+
+            if (itemType) reqURL += `?type=${itemType.value}`
+            if (raceSelector.value) reqURL += `&race=${raceSelector.value}`;
+
+        }
+        console.log(reqURL)
         xhr.open('GET', reqURL, true);
         xhr.send();
         xhr.onloadend = () => {
@@ -30,14 +56,23 @@ loadBtn.addEventListener('click', () => {
         let resp = JSON.parse(response);
         resp.forEach(item => {
             let outputItem = document.createElement('li');
+            outputItem.classList.add(`i${item.id}`)
             outputItem.classList.add('outputItem')
-            outputItem.innerHTML = `<div>${item.name}</div>
-                                    <div>ID: ${item.id}</div>
-                                    <div>Min sell price: ${item.lowestSellPrice}</div>
-                                    <div>Max buy price: ${item.lowestSellPrice}</div>`
+            outputItem.innerHTML = `<div class="itemName">${item.name}</div>
+                                    <div class="itemID">ID: ${item.id}</div>
+                                    <div class="minSell">Min sell price: ${item.lowestSellPrice}</div>
+                                    <div class="maxBuy">Max buy price: ${item.lowestSellPrice}</div>`
             output.appendChild(outputItem);
+
+            outputItem.addEventListener('click', () =>{
+
+            })
         })
 
     })
 
 })
+
+let outputitem = document.querySelectorAll('.outputItem');
+
+
